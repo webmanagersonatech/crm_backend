66c27bf26f2b327ebe9ef4839043dc9495f0f44b
vinoth <webdeveloper2@sonatech.ac.in>
2026-02-27 16:32:46 +0530

    stage

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://160.187.54.80:3000', 'https://hika.sonastar.com','https://hikaapp.sonastar.com','https://hikaenq.sonastar.com/', 'http://localhost:3001', 'http://160.187.54.80:3001', 'http://160.187.54.80:3002', 'http://localhost:3002'], // frontend URLs
+    origin: ['http://localhost:3000', 'http://160.187.54.80:3000', 'https://hika.sonastar.com','https://hikaapp.sonastar.com','https://hikaenq.sonastar.com', 'http://localhost:3001', 'http://160.187.54.80:3001', 'http://160.187.54.80:3002', 'http://localhost:3002'], // frontend URLs
     credentials: true,
 }));
 
