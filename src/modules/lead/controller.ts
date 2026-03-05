5ad21fc402f50a577dc3237ed8f31d3db047c135
vinoth <webdeveloper2@sonatech.ac.in>
2026-03-05 18:01:05 +0530

    push

--- src/modules/lead/controller.ts
+++ src/modules/lead/controller.ts
@@ -686,10 +686,11 @@ export const listLeads = async (req: AuthRequest, res: Response) => {
     } else if (user.role === "admin") {
       filter.instituteId = user.instituteId;
     } else if (user.role === "user") {
-      filter = {
-        instituteId: user.instituteId,
-        // createdBy: user.id
-      };
+      filter.instituteId = user.instituteId;
+
+      if (user.userType === "third_party") {
+        filter.createdBy = user.id;
+      }
     }
 
     // 🔹 Optional filters
